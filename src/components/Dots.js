40901c2483c167627cc0d0db5ab4c0365a512bda
Valerii from earth <lvaldev.code@gmail.com>
2022-10-02 23:46:58 +0200

    add dots animation

--- src/components/Dots.js
+++ src/components/Dots.js
@@ -3,14 +3,16 @@ import styled from "styled-components";
 const DotsStyled = styled.div`
 	--size: ${({ size }) => (size === "sm" ? 15 : size === "md" ? 30 : size === "lg" ? 45 : 30)}px;
 	--speed: ${({ speed }) => (speed >= 500 && speed <= 3000 ? speed : 1500)}ms;
-	--color: ${({ color }) => color || "white"};
+	--color: ${({ color }) => color};
 	display: flex;
-	gap: 10px;
-	height: 75px;
+	align-items: center;
+	gap: calc(var(--size) / 3);
+	height: 100px;
 	--delay: 0.15s;
 	span {
 		height: var(--size);
 		width: var(--size);
+		margin-bottom: calc(var(--size) * 2.33);
 		border-radius: 50%;
 		animation: balls var(--speed) ease-in-out infinite;
 		&:nth-child(1) {
@@ -33,20 +35,21 @@ const DotsStyled = styled.div`
 		0%,
 		80%,
 		100% {
-			box-shadow: 0 40px 0 0px #fff;
+			box-shadow: 0 calc(var(--size) * 1.33) 0 0px var(--color, white);
 		}
 		40% {
-			box-shadow: 0 40px 0 -20px #fff;
+			box-shadow: 0 calc(var(--size) * 1.33) 0 calc(var(--size) * -0.67) var(--color, white);
 		}
 	}
 `;
 
-export default function Dots() {
+export default function Dots({ size, speed, color, count }) {
+	let childs = [..."s".repeat(count >= 3 && count <= 5 ? count : 3)];
 	return (
-		<DotsStyled>
-			<span></span>
-			<span></span>
-			<span></span>
+		<DotsStyled size={size} speed={speed} color={color}>
+			{childs.map((s, i) => (
+				<span key={i}></span>
+			))}
 		</DotsStyled>
 	);
 }
