f86b0f95bd045129ba84271070858092b8772b25
Valerii from earth <lvaldev.code@gmail.com>
2022-10-03 15:09:40 +0200

    add text component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Bars from "./components/Bars";
 import Dots from "./components/Dots";
 import Card from "./components/Card";
 import Ring from "./components/Ring";
+import Text from "./components/Text";
 
 function App() {
 	return (
@@ -15,14 +16,16 @@ function App() {
 				<div style={{ flex: "0 0 110px" }}>Dots</div>
 				<div>Card</div>
 				<div>Ring</div>
+				<div style={{ flex: "0 0 130px" }}>Text</div>
 			</div>
 			<div className='preview'>
 				<Circle color='hsl(0,50%,70%)' size='md' speed='1000' />
 				<Frame color='hsl(36,50%,70%)' _width={7} size='md' speed='1100' />
 				<Bars color='hsl(72,50%,70%)' _width={5} size='md' count={5} speed='1500' />
 				<Dots color='hsl(108,50%,70%)' size='md' speed='1500' count={3} />
-				<Card color='hsl(144,50%,50%)' color2='hsl(180,70%,50%)' size='md' speed='1200' />
+				<Card color='hsl(252,50%,70%)' color2='hsl(180,70%,50%)' size='md' speed='1200' />
 				<Ring color='hsl(216,50%,80%)' size='md' speed='2000' _width={10} />
+				<Text color='hsl(144,50%,50%)' size='sm' speed='1200' text='Loading...' uppercase />
 			</div>
 		</div>
 	);
