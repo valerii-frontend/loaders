232e2b5e09578fb0091eb7166f6213f545faee8d
Valerii from earth <lvaldev.code@gmail.com>
2022-10-23 21:51:16 +0200

    add ballance doc

--- src/stories/Ballance.stories.js
+++ src/stories/Ballance.stories.js
@@ -7,11 +7,40 @@ export default {
     size: {
       options: ["sm", "md", "lg"],
       control: { type: "radio" },
+      defaultValue: "md",
+      description: "Loader size",
+      table: {
+        defaultValue: { summary: "md" },
+        type: { summary: null },
+      },
     },
     color: {
-      control: { type: "color" },
+      defaultValue: "black",
+      control: {
+        type: "color",
+      },
+      table: {
+        defaultValue: { summary: "white" },
+        type: { summary: "EXAMPLE: black, rgba(0,0,0,50%), #000" },
+      },
+      description: "Loader color, support is every CSS color format",
     },
-    speed: { control: { type: "range", min: 500, max: 3000, step: 100 } },
+    speed: {
+      control: { type: "range", min: 500, max: 3000, step: 100 },
+      defaultValue: 1200,
+      description: "Animation speed from `500ms` to `3000ms`",
+      table: {
+        defaultValue: { summary: 1200 },
+        type: { summary: null },
+      },
+    },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: "Ballance loader component preview and props description",
+      },
+    },
   },
 };
 
